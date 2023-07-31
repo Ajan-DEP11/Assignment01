3cd14f7669cf056af1b12974e11a79142b19cab3
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-07-31 23:00:12 +0530

    Second commit

--- Demo3.java
+++ Demo3.java
@@ -1,6 +1,15 @@
 public class Demo3 {
     public static void main(String[] args){
-        
+
+        int AreaCode = 123;
+        int MiddleNum = 456;
+        int LastNum = 7809;
+
+        final String COLOR = "\033[32;1m";
+        final String RESET = "\033[30;0m";
+
+        System.out.printf("Phone Number - (%s%s%s)%s-%s\n",COLOR,AreaCode,RESET,MiddleNum,LastNum);
+
     }
     
 }
