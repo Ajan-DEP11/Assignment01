7c3ce865c12d9110183f43bb20c58f40aff9404e
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-08-01 22:33:40 +0530

    Complete Task 6-A

--- Demo6_task_a.java
+++ Demo6_task_a.java
@@ -1,7 +1,15 @@
 public class Demo6_task_a{
     public static void main(String[] args){
 
-        
+        final String Red = "\u001B[41m";
+        final String Green = "\u001B[42m";
+        final String Reset = "\u001B[0m";
+
+        String NUM1= "[      5" ;
+        String Num2 = "0%     ]";
+
+
+        System.out.printf("%s%s%s%s%s\n",Red,NUM1,Green,Num2,Reset);
         
     }
     
