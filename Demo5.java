10775be9bba2190fe525a9e8a2a3e1de13cf09d0
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-08-01 22:01:25 +0530

    Complete task 5

--- Demo5.java
+++ Demo5.java
@@ -1,5 +1,15 @@
 public class Demo5 {
     public static void main(String[] args){
+
+        int Num1 = 978;
+        int Num2 = 3;
+        int Num3 = 16;
+        int Num4 = 1484100;
+
+        
+
+        System.out.printf("\033[32;1m%s\033[32;0m-\033[33;1m%s\033[30;0m-\033[35;1m%s\033[30;0m-\033[34;1m%s\033[30;0m\n",Num1,Num2,Num3,Num4);
+        
         
     }
     
