52afe0d8aa0072bc9f248020f52ffd565e0b51e0
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-08-01 21:01:16 +0530

    Complete task 4

--- Demo4.java
+++ Demo4.java
@@ -1,5 +1,16 @@
 public class Demo4 {
     public static void main(String[] args){
+
+        int Date = 28;
+        int Month = 12;
+        int Year = 2023;
+
+        int hour = 23;
+        int minute = 59;
+        int second = 59;
+
+        System.out.printf("\033[32;1m%s\033[32;0m/\033[33;1m%s\033[30;0m/\033[35;1m%s\033[30;0m%s",Month,Date,Year," ");
+        System.out.printf("\033[34;1m%s\033[32;0m:\033[36;1m%s\033[30;0m:\033[37;1m%s\033[30;0m\n",hour,minute,second);
         
     }
     
